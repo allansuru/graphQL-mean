45bbc7243d7e894aafe4c78add82d4a54234fd26
SuRu <allansuru@hotmail.com>
2020-10-27 17:10:31 -0300

    melhoriazinha

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -12,13 +12,13 @@ module.exports = {
             })
         }
     },
-    createQuote: async function ({ quoteInput }) {
-        const quote = new Quote({
-            quote: quoteInput.quote,
-            author: quoteInput.author
+    createQuote: async function ({ quoteInput: { quote, author } }) {
+        const quoteData = new Quote({
+            quote,
+            author
         });
 
-        const createdQuote = await quote.save();
+        const createdQuote = await quoteData.save();
         return {
             ...createdQuote._doc,
             _id: createdQuote._id.toString(),
